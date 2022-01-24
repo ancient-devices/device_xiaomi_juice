b72ebe2a4e3460630e1630487699c1710c0bc7df
ganomin <szymon.wirkus12@gmail.com>
2022-01-24 19:31:37 +0100

    juice: nfc: Import NFC configs
    * from qssi-user-11-RKQ1.201004.002-21.11.3-release-keys
    
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- device.mk
+++ device.mk
@@ -142,6 +142,11 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.2 \
     android.hardware.neuralnetworks@1.2.vendor
 
+# NFC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/nfc/libnfc-hal-st.conf:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/libnfc-hal-st.conf
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
