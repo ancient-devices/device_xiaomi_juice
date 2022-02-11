c1a47c7037cfe05f05bef194ae858653a6fcb001
KuatoDev <kuatodev@gmail.com>
2022-02-11 20:47:25 +0700

    Juice : Initial JuiceParts

---
+++ JuiceParts/src/org/lineageos/settings/device/BootReceiver.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018-2019 The Xiaomi-SDM660 Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package org.lineageos.settings.device;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+
+import java.lang.Math.*;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+
+        FileUtils.setValue(DeviceSettings.VIBRATION_STRENGTH_PATH, Settings.Secure.getInt(
+                context.getContentResolver(), DeviceSettings.PREF_VIBRATION_STRENGTH, 80) / 100.0 * (DeviceSettings.MAX_VIBRATION - DeviceSettings.MIN_VIBRATION) + DeviceSettings.MIN_VIBRATION);
+        context.startService(new Intent(context, DiracService.class));
+    }
+}
