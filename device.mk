0c923891092fa0d99ef3392c36eb0a88684f94bb
indraadp <indraproject777@gmail.com>
2022-04-05 22:04:51 +0700

    Revert "juice: Drop prebuilt GCamGo"
    This reverts commit aca5b4459acce476f3bc61a684975f29c68a3bfa.

--- device.mk
+++ device.mk
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
+# Google Camera
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
